b45d784373f8ea161d7ad0bb342d2bd645ad756f
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-30 12:20:49 +0300

    make build

---
+++ index.6e7637d9.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){var l=e.target.closest(".list-item__link");l&&(e.preventDefault(),t.src=l.href)});
+//# sourceMappingURL=index.6e7637d9.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const gallery = document.querySelector(".gallery__list");
+const mainImage = document.getElementById("largeImg");
+gallery.addEventListener("click", (e)=>{
+    const imageLink = e.target.closest(".list-item__link");
+    if (!imageLink) return;
+    e.preventDefault();
+    mainImage.src = imageLink.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
